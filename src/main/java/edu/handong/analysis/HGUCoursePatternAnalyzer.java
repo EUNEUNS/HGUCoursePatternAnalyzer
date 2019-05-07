1a04f35959e9f37ce0c19cc8b6157c711d342ad2
user <user@LAPTOP-3ISCULAG>
2019-05-07 20:33:40 +0900

    add students and course class

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -4,8 +4,8 @@ import edu.handong.analysis.datamodel.Course;
 import edu.handong.analysis.datamodel.Student;
 
 public class HGUCoursePatternAnalyzer {
-	
-	String[] lines = {	"1999-1, JC Nam, Java Programming",
+	private String[] lines = {	
+						"1999-1, JC Nam, Java Programming",
 						"1999-2, JC Nam, Programming Language Theory",
 						"1999-1, JC Nam, Data Structures",
 						"2001-1, JC Nam, Database Systems",
@@ -18,18 +18,15 @@ public class HGUCoursePatternAnalyzer {
 						"2019-1, SJ Kim, Logic Design",
 						"2019-1, SJ Kim, Algorithm Analysis",
 						};
-
-	int numOfStudents;
-	int numOfCourses;
-	Student[] students;
-	Course[] courses;
-	
+	private  int numOfStudents;
+	private  int numOfCourses;
+	private  Student[] students;
+	private  Course[] courses;
 	/**
 	 * This method runs our analysis logic to get the list of student and course names from lines.
 	 * @param args
 	 */
 	public void run(String[] args) {
-		
 		numOfStudents = Integer.parseInt(args[0]);
 		numOfCourses = Integer.parseInt(args[1]);
 	
@@ -37,53 +34,81 @@ public class HGUCoursePatternAnalyzer {
 		
 		System.out.println("Number of All Students: " + numOfStudents);
 		for(Student student: students) {
+			if(student!=null)
 			System.out.println(student.getName());
 		}
 		
 		courses = initiateCourseArrayFromLines(lines);
 		System.out.println("Number of All Courses: " + numOfCourses);
 		for(Course course: courses) {
+			if(course!=null) {
 			System.out.println(course.getCourseName());
 		}
-		
+		}
 	}
-
+	
 	/**
 	 * This method returns a Student array to initiate the field, students, from lines.
 	 * @param lines
 	 * @return
 	 */
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
-		
-		// TODO: implement this method
-		
-		
-		return null;
+		Student [] array1 = new Student[20];
+		for(int i=0;i<lines.length;i++) {
+			
+			String oneline = lines[i];
+			String name1 = oneline.split("\\s*,\\s*")[1];
+			Student stu1;
+			stu1 = new Student(name1);		
+			if (studentExist(array1, stu1)!=true) {
+			
+				array1[i] = stu1;
+			
+		}
+		}		
+		return array1;
 	}
 
 	/**
-	 * This method check if there is the same name of the second arugement in the array, students
+	 * This method check if there is the same name of the second argument in the array, students
 	 * @param students
 	 * @param student
 	 * @return boolean
 	 */
-	private boolean studentExist(Student[] students, Student student) {
+	private boolean studentExist(Student[] students, Student student) 
+	{
 		
+		for (Student stu : students )
+		{
+			if (stu!=null) {
+			if (stu.getName().equals(student.getName()))
+			{
+		return true;
+			}
+		}
+		}
 		// TODO: implement this method
-
 		return false;
 	}
-	
 	/**
 	 * This method returns a Course array to initiate the field, courses, from lines.
 	 * @param lines
 	 * @return
 	 */
 	private Course[] initiateCourseArrayFromLines(String[] lines) {
-		
+		Course[] array2 =new Course[12];
+		for(int i=0;i<lines.length;i++) {
+			String oneline = lines[i];
+			String tmp = oneline.split("\\s*,\\s*")[2];
+			Course courcour ;
+			courcour = new Course(tmp);
+			if(courseExist(array2, courcour)!=true) {
+			array2[i]= new Course(tmp);
+			
+		}
+		}
 		// TODO: implement this method
-		
-		return null;
+		return array2;
 	}
 
 	/**
@@ -93,10 +118,17 @@ public class HGUCoursePatternAnalyzer {
 	 * @return boolean
 	 */
 	private boolean courseExist(Course[] courses, Course course) {
-		
-		// TODO: implement this method
-
-		return false;
-	}
-
+		for (Course cour : courses)
+		{
+			if(cour!=null) {
+			if(cour.getCourseName().contentEquals(course.getCourseName()))
+			{
+				return true;
+			}
+		}
+		}
+			return false;
+		}
 }
+		
+	
---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,14 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	private String name;
+	public Course (String name) {
+		this.name = name;
+	}
+	public String getCourseName() {
+	
+	return name;
+}
+
+}
+
